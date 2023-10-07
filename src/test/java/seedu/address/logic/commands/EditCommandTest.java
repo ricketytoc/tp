17baa7457151f30b32693cd6ce2267f30000a98c
Chen-Kuei <rubychen0427@gmail.com>
2023-10-08 00:45:08 +0800

    Modify redundant changes

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -71,8 +71,8 @@ public class EditCommandTest {
         String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson));
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-
         expectedModel.setPerson(lastPerson, editedPerson);
+
         assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
     }
 
