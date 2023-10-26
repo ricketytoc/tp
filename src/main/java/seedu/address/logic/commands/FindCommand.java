e441d4beba60b9f6b1a786075f27c21ec70e1da5
ricketytoc <122388382+ricketytoc@users.noreply.github.com>
2023-10-26 22:58:40 +0800

    Fix a line that was too long

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -33,7 +33,8 @@ public class FindCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Except for Salary attribute, finds all persons whose "
             + "attributes contain the keyword (case-insensitive) specified for those attributes and displays them as a "
             + "list with index numbers. For Salary, finds all persons whose salary is within a range instead. "
-            + "Upper bound of the range should be less than the maximum allowed salary which is " + Salary.MAXIMUM_SALARY
+            + "Upper bound of the range should be less than the maximum allowed salary which is "
+            + Salary.MAXIMUM_SALARY
             + ".\n"
             + "Parameters: "
             + "[" + PREFIX_ID + "ID] " + " [" + PREFIX_NAME + "NAME] " + " [" + PREFIX_PHONE + "PHONE] "
