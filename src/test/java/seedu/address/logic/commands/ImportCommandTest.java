9a631cc9b29f63c4557bab278e521c76173dec14
aexolate <20797128+aexolate@users.noreply.github.com>
2023-11-08 15:28:20 +0800

    Add import command tests

--- src/test/java/seedu/address/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -80,6 +80,45 @@ class ImportCommandTest {
     }
 
     @Test
+    public void execute_invalidPath_throwsException() throws Exception {
+        // Root folder with forward slash
+        Path invalidPath = Path.of("/");
+        ImportCommand importCommand = new ImportCommand(invalidPath);
+        assertCommandFailure(importCommand, model, ImportCommand.MESSAGE_INVALID_FILE_PATH);
+
+        // Root folder with backward slash
+        invalidPath = Path.of("\\");
+        importCommand = new ImportCommand(invalidPath);
+        assertCommandFailure(importCommand, model, ImportCommand.MESSAGE_INVALID_FILE_PATH);
+
+        // Nested folder (truncated slash at the end)
+        invalidPath = Path.of("/a/b/c/d/");
+        importCommand = new ImportCommand(invalidPath);
+        assertCommandFailure(importCommand, model, ImportCommand.MESSAGE_INVALID_FILE_PATH);
+
+        // Missing data type
+        invalidPath = Path.of("/cs2103-employee-manager-tmp");
+        importCommand = new ImportCommand(invalidPath);
+        assertCommandFailure(importCommand, model, ImportCommand.MESSAGE_INVALID_FILE_PATH);
+
+        // Missing data type
+        // Note: the last '/' will be truncated from the path
+        invalidPath = Path.of("/cs2103-employee-manager-tmp/");
+        importCommand = new ImportCommand(invalidPath);
+        assertCommandFailure(importCommand, model, ImportCommand.MESSAGE_INVALID_FILE_PATH);
+
+        // Nested folder
+        invalidPath = Path.of("/a/b");
+        importCommand = new ImportCommand(invalidPath);
+        assertCommandFailure(importCommand, model, ImportCommand.MESSAGE_INVALID_FILE_PATH);
+
+        // Nested folder
+        invalidPath = Path.of("\\a\\b\\c");
+        importCommand = new ImportCommand(invalidPath);
+        assertCommandFailure(importCommand, model, ImportCommand.MESSAGE_INVALID_FILE_PATH);
+    }
+
+    @Test
     public void equals() {
         Path firstFilePath = Path.of("./data/persons.json");
         Path secondFilePath = Path.of("./data/persons2.json");
