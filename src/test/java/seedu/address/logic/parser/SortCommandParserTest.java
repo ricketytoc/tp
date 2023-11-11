6f48c39ada48663ae9e0078f8f4bb316beeb004f
aexolate <20797128+aexolate@users.noreply.github.com>
2023-11-11 18:30:06 +0800

    Improve code quality

--- src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -24,6 +24,8 @@ import seedu.address.model.person.comparators.SalaryComparator;
 
 public class SortCommandParserTest {
 
+    private static final String INVALID_PREFIX = "invalid/";
+
     private final SortCommandParser parser = new SortCommandParser();
 
     @Test
@@ -35,32 +37,39 @@ public class SortCommandParserTest {
 
     @Test
     public void parse_invalidArg_throwsParseException() {
-        assertParseFailure(parser, " invalid/",
+        assertParseFailure(parser, " " + INVALID_PREFIX,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_validArgs_returnsSortCommand() {
         SortCommand expectedSortCommand = new SortCommand(new DepartmentComparator());
-        assertParseSuccess(parser, " " + PREFIX_DEPARTMENT.getPrefix(), expectedSortCommand);
+        assertParseSuccess(parser, " " + PREFIX_DEPARTMENT, expectedSortCommand);
 
         expectedSortCommand = new SortCommand(new EmailComparator());
-        assertParseSuccess(parser, " " + PREFIX_EMAIL.getPrefix(), expectedSortCommand);
+        assertParseSuccess(parser, " " + PREFIX_EMAIL, expectedSortCommand);
 
         expectedSortCommand = new SortCommand(new IdComparator());
-        assertParseSuccess(parser, " " + PREFIX_ID.getPrefix(), expectedSortCommand);
+        assertParseSuccess(parser, " " + PREFIX_ID, expectedSortCommand);
 
         expectedSortCommand = new SortCommand(new NameComparator());
-        assertParseSuccess(parser, " " + PREFIX_NAME.getPrefix(), expectedSortCommand);
+        assertParseSuccess(parser, " " + PREFIX_NAME, expectedSortCommand);
 
         expectedSortCommand = new SortCommand(new PhoneComparator());
-        assertParseSuccess(parser, " " + PREFIX_PHONE.getPrefix(), expectedSortCommand);
+        assertParseSuccess(parser, " " + PREFIX_PHONE, expectedSortCommand);
 
         expectedSortCommand = new SortCommand(new RoleComparator());
-        assertParseSuccess(parser, " " + PREFIX_ROLE.getPrefix(), expectedSortCommand);
+        assertParseSuccess(parser, " " + PREFIX_ROLE, expectedSortCommand);
 
         expectedSortCommand = new SortCommand(new SalaryComparator());
-        assertParseSuccess(parser, " " + PREFIX_SALARY.getPrefix(), expectedSortCommand);
+        assertParseSuccess(parser, " " + PREFIX_SALARY, expectedSortCommand);
+
+        // Ignore values in the prefix
+        expectedSortCommand = new SortCommand(new SalaryComparator());
+        assertParseSuccess(parser, " " + PREFIX_SALARY + PREFIX_ROLE, expectedSortCommand);
+
+        expectedSortCommand = new SortCommand(new SalaryComparator());
+        assertParseSuccess(parser, " " + PREFIX_SALARY + "5000", expectedSortCommand);
     }
 
     @Test
